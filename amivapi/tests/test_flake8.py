135a22a7539258dcd9783b7a0040f8a36ef4b733
fubu <el.fubinho@gmail.com>
2014-11-10 14:57:39 +0100

    tests: remove unused amivapi.common module

--- amivapi/tests/test_flake8.py
+++ amivapi/tests/test_flake8.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from flake8.engine import get_style_guide
 from flake8.main import print_report
 
-from common import ROOT_PATH
+from amivapi.settings import ROOT_DIR
 
 
 class TestFlake8(TestCase):
@@ -10,6 +10,6 @@ class TestFlake8(TestCase):
     def test_flake8(self):
 
         flake8 = get_style_guide(exclude=['.tox', 'build'])
-        report = flake8.check_files([ROOT_PATH])
+        report = flake8.check_files([ROOT_DIR])
         exit_code = print_report(report, flake8)
         self.assertTrue(exit_code == 0)
