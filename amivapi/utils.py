9d9f1821f9b051ab814fc062ed83bf603c049e5e
hermannsblum <hermann@public-docking-hg-2626.ethz.ch>
2014-12-10 10:21:08 +0100

    added library for parsing and other stuff

---
+++ amivapi/utils.py
@@ -0,0 +1,54 @@
+import datetime as dt
+import json
+from eve.methods.common import payload
+
+
+#if the data is in json format it will not be parsed into request.form
+#therefore we make one object for both cases which we can just use
+def parse_data(request):
+    with request:
+        data = payload()
+    return data
+
+
+class DateTimeDecoder(json.JSONDecoder):
+
+    def __init__(self, *args, **kargs):
+        json.JSONDecoder.__init__(
+            self, object_hook=self.dict_to_object,
+            *args, **kargs
+        )
+
+    def dict_to_object(self, d):
+        if '__type__' not in d:
+            return d
+
+        type = d.pop('__type__')
+        try:
+            dateobj = dt(**d)
+            return dateobj
+        except:
+            d['__type__'] = type
+            return d
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    """ Instead of letting the default encoder convert datetime to string,
+        convert datetime objects into a dict, which can be decoded by the
+        DateTimeDecoder
+    """
+
+    def default(self, obj):
+        if isinstance(obj, dt.datetime):
+            return {
+                '__type__': 'datetime',
+                'year': obj.year,
+                'month': obj.month,
+                'day': obj.day,
+                'hour': obj.hour,
+                'minute': obj.minute,
+                'second': obj.second,
+                'microsecond': obj.microsecond,
+            }
+        else:
+            return json.JSONEncoder.default(self, obj)
