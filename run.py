0b6838e8eb646b743d131e99ec2ca1090235f12c
Leonidaz0r <bconrad@student.ethz.ch>
2014-12-16 22:19:26 +0100

    Missing file from last commit

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from amivapi import bootstrap
 
 if __name__ == '__main__':
-    app = bootstrap.create_app("development", create_db=True)
+    app = bootstrap.create_app("development")
     app.run()
