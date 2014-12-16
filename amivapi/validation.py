ced3021b735740863753845a33cce8b8cc38fa15
Alexander Dietmüller <alexander@dietmueller.de>
2014-12-16 19:37:52 +0100

    Added custom validator for media type.

---
+++ amivapi/validation.py
@@ -0,0 +1,24 @@
+"""
+    amivapi.validation
+    ~~~~~~~~~~~~~~~~~~~~~~~
+    This extends the currently used validator to accept 'media' type
+"""
+
+from eve.io.sql import ValidatorSQL
+from werkzeug.datastructures import FileStorage
+
+
+class ValidatorAMIV(ValidatorSQL):
+    """ A cerberus.Validator subclass adding the `unique` constraint to
+    Cerberus standard validation. For documentation please refer to the
+    Validator class of the eve.io.mongo package.
+    """
+
+    def _validate_type_media(self, field, value):
+        """ Enables validation for `media` data type.
+        :param field: field name.
+        :param value: field value.
+        .. versionadded:: 0.3
+        """
+        if not isinstance(value, FileStorage):
+            self._error(field, "file was expected, got '%s' instead." % value)
