f98ec7413b872a0988f4004419960440f017ecd2
hermannsblum <hermann@Hermanns-MacBook-Air-2.local>
2014-12-19 11:51:16 +0100

    flake8 fixes

---
+++ amivapi/tests/test_events.py
@@ -0,0 +1,48 @@
+import datetime as dt
+
+from amivapi import models
+from amivapi.tests import util
+
+from amivapi.settings import DATE_FORMAT
+
+import json
+
+
+class EventTest(util.WebTestNoAuth):
+
+    def test_a_schema(self):
+        start = dt.datetime.today() + dt.timedelta(days=2)
+        self.api.post("/events", data={
+            'title': "Awesome Test Event",
+            'time_start': start.strftime(DATE_FORMAT),
+            'is_public': True,
+            'price': '0',
+            'spots': 10,
+            'time_register_start': dt.datetime.now().strftime(DATE_FORMAT),
+            'time_register_end': start.strftime(DATE_FORMAT),
+            'additional_fields': json.dumps({
+                'department': {
+                    'type': 'lol',
+                }
+            })
+        }, status_code=422)
+        event_count = self.db.query(models.Event).count()
+        self.assertEquals(event_count, 0)
+
+        #make new event with schema that is now json
+        self.api.post("/events", data={
+            'title': "Awesome Test Event",
+            'time_start': start.strftime(DATE_FORMAT),
+            'is_public': True,
+            'price': '0',
+            'spots': 10,
+            'time_register_start': dt.datetime.now().strftime(DATE_FORMAT),
+            'time_register_end': start.strftime(DATE_FORMAT),
+            'additional_fields': 'lol ( %s)' % json.dumps({
+                'department': {
+                    'type': 'lol',
+                }
+            })
+        }, status_code=422)
+        event_count = self.db.query(models.Event).count()
+        self.assertEquals(event_count, 0)
---
+++ amivapi/tests/test_forwards.py
@@ -0,0 +1,71 @@
+from amivapi import models
+from amivapi.tests import util
+
+
+class ForwardTest(util.WebTestNoAuth):
+
+    def test_a_assign_registered(self):
+        user = self.new_user()
+        forward = self.new_forward()
+
+        # forward non-existing user
+        self.api.post("/forwardusers", data={
+            'user_id': user.id + 1,
+            'forward_id': forward.id,
+        }, status_code=422)
+        forwarduser_count = self.db.query(models.ForwardUser).count()
+        self.assertEquals(forwarduser_count, 0)
+
+        # forward non-existing forward
+        self.api.post("/forwardusers", data={
+            'user_id': user.id,
+            'forward_id': forward.id + 1,
+        }, status_code=422)
+        forwarduser_count = self.db.query(models.ForwardUser).count()
+        self.assertEquals(forwarduser_count, 0)
+
+        # do everything right
+        self.api.post("/forwardusers", data={
+            'user_id': user.id,
+            'forward_id': forward.id,
+        }, status_code=201)
+        forwarduser_count = self.db.query(models.ForwardUser).count()
+        self.assertEquals(forwarduser_count, 1)
+
+    def test_b_assign_unregistered(self):
+        email = "test-mail@amiv.ethz.ch"
+        forward = self.new_forward()
+
+        # forward to non-email-address
+        self.api.post("/forwardaddresses", data={
+            'address': 'fakeaddress',
+            'forward_id': forward.id,
+        }, status_code=422)
+        forwards = self.db.query(models.ForwardAddress)
+        self.assertEquals(forwards.count(), 0)
+
+        # forwards to not existing forward
+        self.api.post("/forwardaddresses", data={
+            'address': email,
+            'forward_id': forward.id + 7,
+        }, status_code=422)
+        forwards = self.db.query(models.ForwardAddress)
+        self.assertEquals(forwards.count(), 0)
+
+        # do everything right and look if it get's added to Confirm
+        self.api.post("/forwardaddresses", data={
+            'address': email,
+            'forward_id': forward.id,
+        }, status_code=202)
+        forwards = self.db.query(models.ForwardAddress)
+        self.assertEquals(forwards.count(), 0)
+        confirms = self.db.query(models.Confirm)
+        self.assertEquals(confirms.count(), 1)
+
+        # get token and confirm the address
+        # TODO: test email system
+        self.api.post("/confirms", data={
+            'token': forwards.first().token
+        }, status_code=201)
+        self.assertEquals(forwards.count(), 1)
+        self.assertEquals(confirms.count(), 0)
