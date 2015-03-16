57286c6003abbd0dbc679d3583fd740e6535f04d
Leonidaz0r <bconrad@student.ethz.ch>
2015-03-16 19:35:02 +0100

    Added AGPL license, content encoding, updated setup.py

--- amivapi/tests/test_file_endpoint.py
+++ amivapi/tests/test_file_endpoint.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# AMIVAPI test_file_endpoint.py
+# Copyright (C) 2015 AMIV an der ETH, see AUTHORS for more details
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import util
 
 
