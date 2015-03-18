b36427f1ee82f08032f7066bd1167e08627faa87
Leonidaz0r <bconrad@student.ethz.ch>
2015-03-18 16:18:56 +0100

    Shortened license header to important info

--- amivapi/tests/test_authentification.py
+++ amivapi/tests/test_authentification.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# license: AGPLv3, see LICENSE for details. In addition we strongly encourage
+#          you to buy us beer if we meet and you like the software.
+
 from amivapi.tests import util
 
 
