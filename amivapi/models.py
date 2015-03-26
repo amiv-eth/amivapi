d3366fec4546cfd4dd11c11eca2863196060b13e
Leonidaz0r <bconrad@student.ethz.ch>
2015-03-26 15:41:24 +0100

    Substituted send_no_mail with send_newsletter
    New version is more precise. In addition removed nullable flag, as this
    also modifies the schema

--- amivapi/models.py
+++ amivapi/models.py
@@ -111,7 +111,7 @@ class User(Base):
     email = Column(CHAR(100), nullable=False, unique=True)
     membership = Column(Enum("none", "regular", "extraordinary", "honorary"),
                         nullable=False, default="none", server_default="none")
-    send_no_mail = Column(Boolean, default=False, nullable=False)
+    send_newsletter = Column(Boolean, default=True)
 
     # relationships
     permissions = relationship("Permission", foreign_keys="Permission.user_id",
