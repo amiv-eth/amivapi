f7381747eb46a2c6b3faf67ee1bd4e097433c29d
Leonidaz0r <bconrad@student.ethz.ch>
2015-03-27 03:11:29 +0100

    Fix authentification test

--- amivapi/tests/test_authentification.py
+++ amivapi/tests/test_authentification.py
@@ -10,25 +10,25 @@ class AuthentificationTest(util.WebTest):
 
     def test_invalid_username(self):
         """ Try to login with an unknown username """
-        self.new_user(username="user1")
+        self.new_user(username=u"user1", password=u"user1")
 
-        self.api.post("/sessions", data={'username': "user1\0",
-                                         'password': ''}, status_code=401)
+        self.api.post("/sessions", data={'username': u"user1\0",
+                                         'password': u"user1"}, status_code=401)
 
     def test_no_usernames(self):
         """ Try to login without username """
         self.new_user(username="user1")
 
-        self.api.post("/sessions", data={'password': 'mypw'}, status_code=422)
+        self.api.post("/sessions", data={'password': u'mypw'}, status_code=422)
 
     def test_no_password(self):
         """ Try to login without password """
-        self.new_user(username="user1")
+        self.new_user(username=u"user1")
 
-        self.api.post("/sessions", data={'username': 'user1'}, status_code=422)
+        self.api.post("/sessions", data={'username': u'user1'}, status_code=422)
 
     def test_invalid_token(self):
         """ Try to do a request using invalid token """
         self.new_session()
 
-        self.api.get("/users", token="xxx", status_code=401)
+        self.api.get("/users", token=u"xxx", status_code=401)
