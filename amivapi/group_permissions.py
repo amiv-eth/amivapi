690588c1a473108b4978d39c2f5dea4524a69f6e
NotSpecial <alexander@dietmueller.de>
2015-11-16 17:48:40 +0100

    Added jsonschema to requirements and flake8 fixes
    There should be no flake8 errors in group_permissions anymore

--- amivapi/group_permissions.py
+++ amivapi/group_permissions.py
@@ -7,20 +7,21 @@
     Contains functions for group permissions
 """
 
+
 def create_group_permissions_jsonschema(domain):
-    """ 
+    """
     This is the authentification function called by eve. It will parse
     the send token and determine if it is from a valid user or a know
     apikey.
-        
+
     You should not call this function directly. Use the functions in
     authorization.py instead(have a look at common_authorization()).
-        
+
     :param domain: domain of the api, i.e. a list of endpoints (strings)
-        
+
     :returns: A dictionary which is jsonschema
     """
-    
+
     # Create outer container
     # Properties will be the enpoints
     # additionalProperties has to be false, otherwise all unknown properties
@@ -32,7 +33,7 @@ def create_group_permissions_jsonschema(domain):
         "additionalProperties": False,
         "properties": {}
     }
-    
+
     # Now add endpoints as allowed properties
     # This is the inner container, they are again objects
     for res in domain:
@@ -42,14 +43,14 @@ def create_group_permissions_jsonschema(domain):
             "additionalProperties": False,
             "properties": {}
         }
-        
+
         subschema = schema["properties"][res]["properties"]
-        
+
         # All basic methods covered, just boolean
         for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
             subschema[method] = {
                 "title": 'Permission for %s' % method,
                 "type": "boolean"
-        }
-            
+            }
+
     return schema
