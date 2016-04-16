44ab40ac25d2a192f984369fd4ee29ea2140a3e5
IgorSusmelj <isusmelj@gmail.com>
2016-04-16 13:29:48 +0200

    Adjusted 'events' to fit new strucutre

--- amivapi/events/endpoints.py
+++ amivapi/events/endpoints.py
@@ -120,72 +120,225 @@ class EventSignup(utils.Base):
     _confirmed = Column(Boolean, default=False)
 
 
-def make_eventdomain():
-    """Create domain.
 
-    This is a function so it can be called after models in all modules have
-    been defined.
-    """
-    eventdomain = {}
-
-    eventdomain.update(utils.make_domain(Event))
-    eventdomain.update(utils.make_domain(EventSignup))
-
-    # additional validation
-    eventdomain['events']['schema']['additional_fields'].update({
-        'type': 'json_schema'})
-    eventdomain['events']['schema']['price'].update({'min': 0})
-    eventdomain['events']['schema']['spots'].update({
-        'min': -1,
-        'if_this_then': ['time_register_start', 'time_register_end']})
-    eventdomain['events']['schema']['time_register_end'].update({
-        'dependencies': ['time_register_start'],
-        'later_than': 'time_register_start'})
-    eventdomain['events']['schema']['time_end'].update({
-        'dependencies': ['time_start'],
-        'later_than': 'time_start'})
-
-    # time_end for /events requires time_start
-    eventdomain['events']['schema']['time_end'].update({
-        'dependencies': ['time_start']
-    })
-
-    # event images
-    eventdomain['events']['schema'].update({
-        'img_thumbnail': {'type': 'media', 'filetype': ['png', 'jpeg']},
-        'img_banner': {'type': 'media', 'filetype': ['png', 'jpeg']},
-        'img_poster': {'type': 'media', 'filetype': ['png', 'jpeg']},
-        'img_infoscreen': {'type': 'media', 'filetype': ['png', 'jpeg']}
-    })
-
-    # POST done by custom endpoint
-    eventdomain['eventsignups']['resource_methods'] = ['GET']
-
-    # schema extensions including custom validation
-    eventdomain['eventsignups']['schema']['event_id'].update({
-        'not_patchable': True,
-        'unique_combination': ['user_id', 'email'],
-        'signup_requirements': True})
-    eventdomain['eventsignups']['schema']['user_id'].update({
-        'not_patchable': True,
-        'unique_combination': ['event_id'],
-        'only_self_enrollment_for_event': True})
-    eventdomain['eventsignups']['schema']['email'].update({
-        'not_patchable': True,
-        'unique_combination': ['event_id'],
-        'only_anonymous': True,
-        'email_signup_must_be_allowed': True,
-        'regex': utils.EMAIL_REGEX})
-
-    # Since the data relation is not evaluated for posting, we need to remove
-    # it from the schema TODO: EXPLAIN BETTER
-    del(eventdomain['eventsignups']['schema']['email']['data_relation'])
-    # Remove _email_unreg and _token from the schema since they are only
-    # for internal use and should not be visible
-    del(eventdomain['eventsignups']['schema']['_email_unreg'])
-    del(eventdomain['eventsignups']['schema']['_token'])
-
-    eventdomain['eventsignups']['schema']['additional_fields'].update({
-        'type': 'json_event_field'})
-
-    return eventdomain
+eventdomain = {'events': {'datasource': {'projection': {'_author': 1,
+                                          'additional_fields': 1,
+                                          'allow_email_signup': 1,
+                                          'catchphrase_de': 1,
+                                          'catchphrase_en': 1,
+                                          'description_de': 1,
+                                          'description_en': 1,
+                                          'id': 1,
+                                          'img_banner': 1,
+                                          'img_infoscreen': 1,
+                                          'img_poster': 1,
+                                          'img_thumbnail': 1,
+                                          'location': 1,
+                                          'price': 1,
+                                          'show_announce': 1,
+                                          'show_infoscreen': 1,
+                                          'show_website': 1,
+                                          'signup_count': 1,
+                                          'signups': 1,
+                                          'spots': 1,
+                                          'time_end': 1,
+                                          'time_register_end': 1,
+                                          'time_register_start': 1,
+                                          'time_start': 1,
+                                          'title_de': 1,
+                                          'title_en': 1},
+                           'source': 'Event'},
+            'description': {'fields': {'additional_fields': 'must be provided in form of a JSON-Schema. You can add here fields you want to know from people signing up going further than their email-address',
+                                       'allow_email_signup': 'If False, only AMIV-Members can sign up for this event',
+                                       'price': 'Price of the event as Integer in Rappen.',
+                                       'spots': "For no limit, set to '0'. If no signup required, set to '-1'. Otherwise just provide an integer."},
+                            'general': 'An Event is basically everything happening in the AMIV. All time fields have the format YYYY-MM-DDThh:mmZ, e.g. 2014-12-20T11:50:06Z',
+                            'methods': {'GET': 'You are always allowed, even without session, to view AMIV-Events'}},
+            'embedded_fields': {},
+            'item_lookup': True,
+            'item_lookup_field': '_id',
+            'item_url': 'regex("[0-9]+")',
+            'owner': [],
+            'owner_methods': [],
+            'public_item_methods': ['GET'],
+            'public_methods': ['GET'],
+            'registered_methods': [],
+            'schema': {'_author': {'data_relation': {'embeddable': False,
+                                                     'resource': 'users'},
+                                   'nullable': True,
+                                   'readonly': True,
+                                   'required': False,
+                                   'type': 'objectid',
+                                   'unique': False},
+                       'additional_fields': {'nullable': True,
+                                             'required': False,
+                                             'type': 'json_schema',
+                                             'unique': False},
+                       'allow_email_signup': {'default': False,
+                                              'required': True,
+                                              'type': 'boolean',
+                                              'unique': False},
+                       'catchphrase_de': {'nullable': True,
+                                          'required': False,
+                                          'type': 'string',
+                                          'unique': False},
+                       'catchphrase_en': {'nullable': True,
+                                          'required': False,
+                                          'type': 'string',
+                                          'unique': False},
+                       'description_de': {'nullable': True,
+                                          'required': False,
+                                          'type': 'string',
+                                          'unique': False},
+                       'description_en': {'nullable': True,
+                                          'required': False,
+                                          'type': 'string',
+                                          'unique': False},
+                       'img_banner': {'filetype': ['png', 'jpeg'],
+                                      'type': 'media'},
+                       'img_infoscreen': {'filetype': ['png', 'jpeg'],
+                                          'type': 'media'},
+                       'img_poster': {'filetype': ['png', 'jpeg'],
+                                      'type': 'media'},
+                       'img_thumbnail': {'filetype': ['png', 'jpeg'],
+                                         'type': 'media'},
+                       'location': {'maxlength': 50,
+                                    'nullable': True,
+                                    'required': False,
+                                    'type': 'string',
+                                    'unique': False},
+                       'price': {'min': 0,
+                                 'nullable': True,
+                                 'required': False,
+                                 'type': 'integer',
+                                 'unique': False},
+                       'show_announce': {'default': False,
+                                         'nullable': True,
+                                         'required': False,
+                                         'type': 'boolean',
+                                         'unique': False},
+                       'show_infoscreen': {'default': False,
+                                           'nullable': True,
+                                           'required': False,
+                                           'type': 'boolean',
+                                           'unique': False},
+                       'show_website': {'default': False,
+                                        'nullable': True,
+                                        'required': False,
+                                        'type': 'boolean',
+                                        'unique': False},
+                       'signup_count': {'readonly': True,
+                                        'required': False,
+                                        'type': 'string',
+                                        'unique': False},
+                       'signups': {'data_relation': {'embeddable': True,
+                                                     'resource': 'eventsignups'},
+                                   'type': 'objectid'},
+                       'spots': {'if_this_then': ['time_register_start',
+                                                  'time_register_end'],
+                                 'min': -1,
+                                 'required': True,
+                                 'type': 'integer',
+                                 'unique': False},
+                       'time_end': {'dependencies': ['time_start'],
+                                    'later_than': 'time_start',
+                                    'nullable': True,
+                                    'required': False,
+                                    'type': 'datetime',
+                                    'unique': False},
+                       'time_register_end': {'dependencies': ['time_register_start'],
+                                             'later_than': 'time_register_start',
+                                             'nullable': True,
+                                             'required': False,
+                                             'type': 'datetime',
+                                             'unique': False},
+                       'time_register_start': {'nullable': True,
+                                               'required': False,
+                                               'type': 'datetime',
+                                               'unique': False},
+                       'time_start': {'nullable': True,
+                                      'required': False,
+                                      'type': 'datetime',
+                                      'unique': False},
+                       'title_de': {'nullable': True,
+                                    'required': False,
+                                    'type': 'string',
+                                    'unique': False},
+                       'title_en': {'nullable': True,
+                                    'required': False,
+                                    'type': 'string',
+                                    'unique': False}},
+            'sql_model': Event},
+ 'eventsignups': {'datasource': {'projection': {'_author': 1,
+                                                '_confirmed': 1,
+                                                '_email_unreg': 1,
+                                                '_token': 1,
+                                                'additional_fields': 1,
+                                                'email': 1,
+                                                'event': 1,
+                                                'event_id': 1,
+                                                'id': 1,
+                                                'user': 1,
+                                                'user_id': 1},
+                                 'source': 'EventSignup'},
+                  'description': {'fields': {'additional_fields': "Data-schema depends on 'additional_fields' from the mapped event. Please provide in json-format.",
+                                             'email': 'For registered users, this is just a projection of your general email-address. External users need to provide their email here.',
+                                             'user_id': "To sign up as external user, set 'user_id' to '-1'"},
+                                  'general': 'You can signup here for an existing event inside of the registration-window. External Users can only sign up to public events.'},
+                  'embedded_fields': {},
+                  'item_lookup': True,
+                  'item_lookup_field': '_id',
+                  'item_url': 'regex("[0-9]+")',
+                  'owner': ['user_id'],
+                  'owner_methods': ['GET', 'PATCH', 'DELETE'],
+                  'public_item_methods': [],
+                  'public_methods': [],
+                  'registered_methods': ['POST'],
+                  'resource_methods': ['GET'],
+                  'schema': {'_author': {'data_relation': {'embeddable': False,
+                                                           'resource': 'users'},
+                                         'nullable': True,
+                                         'readonly': True,
+                                         'required': False,
+                                         'type': 'objectid',
+                                         'unique': False},
+                             '_confirmed': {'default': False,
+                                            'nullable': True,
+                                            'required': False,
+                                            'type': 'boolean',
+                                            'unique': False},
+                             'additional_fields': {'nullable': True,
+                                                   'required': False,
+                                                   'type': 'json_event_field',
+                                                   'unique': False},
+                             'email': {'email_signup_must_be_allowed': True,
+                                       'maxlength': 100,
+                                       'not_patchable': True,
+                                       'nullable': True,
+                                       'only_anonymous': True,
+                                       'regex': '^.+@.+$',
+                                       'required': False,
+                                       'type': 'objectid',
+                                       'unique': False,
+                                       'unique_combination': ['event_id']},
+                             'event': {'data_relation': {'embeddable': True,
+                                                         'resource': 'events'},
+                                       'type': 'objectid'},
+                             'event_id': {'data_relation': {'embeddable': False,
+                                                            'resource': 'events'},
+                                          'not_patchable': True,
+                                          'required': True,
+                                          'signup_requirements': True,
+                                          'type': 'objectid',
+                                          'unique': False,
+                                          'unique_combination': ['user_id',
+                                                                 'email']},
+                             'user_id': {'data_relation': {'embeddable': False,
+                                                           'resource': 'users'},
+                                         'not_patchable': True,
+                                         'only_self_enrollment_for_event': True,
+                                         'required': True,
+                                         'type': 'objectid',
+                                         'unique': False,
+                                         'unique_combination': ['event_id']}},
+                  'sql_model': EventSignup}}
