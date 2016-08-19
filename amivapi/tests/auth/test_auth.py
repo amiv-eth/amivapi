abad17349b7c54b3d631a174f76f2c1671275002
NotSpecial <alexander@dietmueller.de>
2016-08-20 00:35:52 +1000

    Restructured auth tests to remove some copied code

--- amivapi/tests/auth/test_auth.py
+++ amivapi/tests/auth/test_auth.py
@@ -4,13 +4,11 @@
 #          you to buy us beer if we meet and you like the software.
 """Tests for auth functions."""
 
-from contextlib import contextmanager
 from base64 import b64encode
 from datetime import datetime, timedelta
 
 from flask import g
 from werkzeug.exceptions import Unauthorized, Forbidden
-from eve.auth import BasicAuth
 
 from amivapi.auth import (
     AmivTokenAuth,
@@ -19,10 +17,11 @@ from amivapi.auth import (
     abort_if_not_public,
     authenticate,
 )
-from amivapi.tests import utils
+from amivapi.tests.utils import WebTest
+from .fake_auth import FakeAuthTest
 
 
-class AmivTokenAuthTest(utils.WebTest):
+class AmivTokenAuthTest(WebTest):
     """Tests for `AmivTokenAuth` default methods."""
 
     def test_amiv_token_auth_authorized(self):
@@ -53,61 +52,8 @@ class AmivTokenAuthTest(utils.WebTest):
         self.assertFalse(auth.has_write_permission(None, None))
 
 
-class FakeAuth(AmivTokenAuth):
-    """Testing auth class that makes it easy to check results."""
-
-    def create_user_lookup_filter(self, user_id):
-        """Simple lookup."""
-        return {'_id': user_id}
-
-    def has_write_permission(self, user_id, item):
-        """Return true if _id field equals user."""
-        return user_id == item['_id']
-
-
-class AuthFunctionTest(utils.WebTest):
+class AuthFunctionTest(FakeAuthTest):
     """Unittests for the auth functions."""
-
-    # Setup and Helper
-
-    def setUp(self):
-        """Setup with fake resources that have only auth.
-
-        - AmivTokenAuth subclass for the resource 'fake'.
-        - A 'BasicAuth' (from Eve) subclass for 'fake_no_amiv'
-        - No auth at all for 'fake_nothing'
-
-        """
-        super(AuthFunctionTest, self).setUp()
-
-        self.app.config['DOMAIN']['fake'] = {
-            'authentication': FakeAuth,
-            # some different methods for public and not public
-            'resource_methods': ['GET', 'POST', 'DELETE'],
-            'public_methods': ['GET', 'POST'],
-            'item_methods': ['GET', 'PATCH', 'DELETE'],
-            'public_item_methods': ['GET', 'PATCH']
-        }
-
-        self.app.config['DOMAIN']['fake_no_amiv'] = {
-            'authentication': BasicAuth}
-
-        self.app.config['DOMAIN']['fake_nothing'] = {
-            'authentication': None}
-
-    @contextmanager
-    def _init_context(self, **g_updates):
-        """Create an app context and fill g with values."""
-        with self.app.app_context():
-            # Defaults - no admins and nothing
-            g.current_token = g.current_session = g.current_user = None
-            g.resource_admin = g.resource_admin_readonly = False
-
-            # Update g
-            for key, value in g_updates.items():
-                setattr(g, key, value)
-
-            yield
 
     # Tests for add_lookup_filter
 
