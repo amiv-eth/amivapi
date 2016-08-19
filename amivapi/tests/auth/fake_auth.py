582b8797606d4fbec0edd6837361b8a94ecc362a
NotSpecial <alexander@dietmueller.de>
2016-08-20 00:35:52 +1000

    Restructured auth tests to remove some copied code

---
+++ amivapi/tests/auth/fake_auth.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#
+# license: AGPLv3, see LICENSE for details. In addition we strongly encourage
+#          you to buy us beer if we meet and you like the software.
+"""Auth test case with a fake auth class added."""
+
+from contextlib import contextmanager
+
+from flask import g
+from eve.auth import BasicAuth
+
+from amivapi.auth import AmivTokenAuth
+from amivapi.tests.utils import WebTest
+
+
+class FakeAuth(AmivTokenAuth):
+    """Testing auth class that makes it easy to check results."""
+
+    def create_user_lookup_filter(self, user_id):
+        """Simple lookup."""
+        return {'_id': user_id}
+
+    def has_write_permission(self, user_id, item):
+        """Return true if _id field equals user."""
+        return user_id == item['_id']
+
+
+class FakeAuthTest(WebTest):
+    """Unittests for the auth functions with fake auth."""
+
+    # Setup and Helper
+
+    def setUp(self):
+        """Setup with fake resources that have only auth.
+
+        - AmivTokenAuth subclass for the resource 'fake'.
+        - A 'BasicAuth' (from Eve) subclass for 'fake_no_amiv'
+        - No auth at all for 'fake_nothing'
+
+        """
+        super(FakeAuthTest, self).setUp()
+
+        self.app.config['DOMAIN']['fake'] = {
+            'authentication': FakeAuth,
+            # some different methods for public and not public
+            'resource_methods': ['GET', 'POST', 'DELETE'],
+            'public_methods': ['GET', 'POST'],
+            'item_methods': ['GET', 'PATCH', 'DELETE'],
+            'public_item_methods': ['GET', 'PATCH']
+        }
+
+        self.app.config['DOMAIN']['fake_no_amiv'] = {
+            'authentication': BasicAuth}
+
+        self.app.config['DOMAIN']['fake_nothing'] = {
+            'authentication': None}
+
+    @contextmanager
+    def _init_context(self, **g_updates):
+        """Create an app context and fill g with values."""
+        with self.app.app_context():
+            # Defaults - no admins and nothing
+            g.current_token = g.current_session = g.current_user = None
+            g.resource_admin = g.resource_admin_readonly = False
+
+            # Update g
+            for key, value in g_updates.items():
+                setattr(g, key, value)
+
+            yield
