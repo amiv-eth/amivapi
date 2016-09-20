e6bcda013606239179d395f665d9f52942bacefa
NotSpecial <alexander@dietmueller.de>
2016-09-20 12:53:35 +0930

    Lots of flake8 fixes

--- amivapi/joboffers/__init__.py
+++ amivapi/joboffers/__init__.py
@@ -7,9 +7,7 @@
 Since there are no hooks or anything everything is just in here.
 """
 
-from amivapi.utils import make_domain
 from amivapi.utils import register_domain
-
 
 
 def init_app(app):
@@ -17,46 +15,49 @@ def init_app(app):
     register_domain(app, jobdomain)
 
 
-
-jobdomain = {'joboffers': {'datasource': {'projection': {'_author': 1,
-                                         'company': 1,
-                                         'description_de': 1,
-                                         'description_en': 1,
-                                         'id': 1,
-                                         'logo': 1,
-                                         'pdf': 1,
-                                         'time_end': 1,
-                                         'title_de': 1,
-                                         'title_en': 1},
-                          'source': 'JobOffer'},
-           'description': {},
-           'item_lookup': True,
-           'item_lookup_field': '_id',
-           'item_url': 'regex("[0-9]+")',
-           'owner': [],
-           'owner_methods': [],
-           'public_item_methods': ['GET'],
-           'public_methods': ['GET'],
-           'registered_methods': [],
-           'schema': {'_author': {'data_relation': {'resource': 'users'},
-                                  'nullable': True,
-                                  'readonly': True,
-                                  'type': 'objectid'},
-                      'company': {'maxlength': 30,
-                                  'nullable': True,
-                                  'type': 'string'},
-                      'description_de': {'nullable': True,
-                                         'type': 'string',
-                                         'unique': False},
-                      'description_en': {'nullable': True,
-                                         'type': 'string'},
-                      'logo': {'filetype': ['png', 'jpeg'],
-                               'type': 'media'},
-                      'pdf': {'filetype': ['pdf'], 'type': 'media'},
-                      'time_end': {'nullable': True,
-                                   'type': 'datetime'},
-                      'title_de': {'nullable': True,
-                                   'type': 'string'},
-                      'title_en': {'nullable': True,
-                                   'type': 'string'}}}}
-
+jobdomain = {'joboffers': {
+    'datasource': {'projection': {'_author': 1,
+                                  'company': 1,
+                                  'description_de': 1,
+                                  'description_en': 1,
+                                  'id': 1,
+                                  'logo': 1,
+                                  'pdf': 1,
+                                  'time_end': 1,
+                                  'title_de': 1,
+                                  'title_en': 1},
+                   'source': 'JobOffer'},
+    'description': {},
+    'item_lookup': True,
+    'item_lookup_field': '_id',
+    'item_url': 'regex("[0-9]+")',
+    'owner': [],
+    'owner_methods': [],
+    'public_item_methods': ['GET'],
+    'public_methods': ['GET'],
+    'registered_methods': [],
+    'schema': {
+        '_author': {'data_relation': {'resource': 'users'},
+                    'nullable': True,
+                    'readonly': True,
+                    'type': 'objectid'},
+        'company': {'maxlength': 30,
+                    'nullable': True,
+                    'type': 'string'},
+        'description_de': {'nullable': True,
+                           'type': 'string',
+                           'unique': False},
+        'description_en': {'nullable': True,
+                           'type': 'string'},
+        'logo': {'filetype': ['png', 'jpeg'],
+                 'type': 'media'},
+        'pdf': {'filetype': ['pdf'],
+                'type': 'media'},
+        'time_end': {'nullable': True,
+                     'type': 'datetime'},
+        'title_de': {'nullable': True,
+                     'type': 'string'},
+        'title_en': {'nullable': True,
+                     'type': 'string'}
+    }
+}}
--- amivapi/purchases/__init__.py
+++ amivapi/purchases/__init__.py
@@ -6,85 +6,60 @@
 
 Since there are no hooks or anything everything is just in here.
 """
-purchasedomain ={
-        'purchases': 
-            {
-                'item_url': 'regex("[0-9]+")', 
-                'description': 
-                    {
-                        'fields': 
-                            {
-                                'slot': 'Slot in the machine which was purchased(different items, which may have different prices).'
-                            },
-                        'general': 'A beer machine or kaffi machine transaction. Users should be able to get beer or kaffi, if their last timestamp is older than one day and they are AMIV members. This resource is used to log their purchases.'
-                    },
-                'item_lookup_field': '_id',
-                'public_item_methods': [], 
-                'registered_methods': [], 
-                'owner_methods': ['GET'], 
-                'datasource': 
-                    {
-                        'source': 'Purchase', 
-                        'projection': 
-                            {
-                                'slot': 1, 
-                                'user_id': 1, 
-                                'timestamp': 1, 
-                                '_author': 1, 
-                                'type': 1, 
-                                'id': 1
-                            }
-                    }, 
-                'owner': ['user_id'], 
-                'item_lookup': True, 
-                'public_methods': [], 
-                'schema': 
-                    {
-                    'slot': 
-                        {
-                            'required': False, 
-                            'unique': False, 
-                            'type': 'integer', 
-                            'nullable': True
-                        }, 
-                    'user_id': 
-                        {
-                            'required': False, 
-                            'unique': False, 
-                            'type': 'integer', 
-                            'nullable': True
-                        }, 
-                    'timestamp': 
-                        {
-                            'required': False, 
-                            'unique': False, 
-                            'type': 'datetime', 
-                            'nullable': True
-                        }, 
-                    '_author': 
-                        {
-                            'readonly': True, 
-                            'unique': False, 
-                            'data_relation': 
-                                {
-                                    'resource': 'users', 
-                                    'embeddable': False
-                                }, 
-                            'nullable': True, 
-                            'required': False, 
-                            'type': 'objectid'
-                        }, 
-                    'type': 
-                        {
-                            'required': False, 
-                            'unique': False, 
-                            'type': 'string', 
-                            'maxlength': 5, 
-                            'nullable': True
-                        }
-                    }
-            }
-    }
+
+from amivapi.utils import register_domain
+
+purchasedomain = {
+    'purchases': {'datasource': {'projection': {'_author': 1,
+                                                'id': 1,
+                                                'slot': 1,
+                                                'timestamp': 1,
+                                                'type': 1,
+                                                'user_id': 1},
+                                 'source': 'Purchase'},
+                  'description': {
+                      'fields': {
+                          'slot': 'Slot in the machine which was '
+                          'purchased(different items, which may have '
+                          'different prices).'},
+                      'general': 'A beer machine or kaffi machine '
+                      'transaction. Users should be able to get beer or '
+                      'kaffi, if their last timestamp is older than one day '
+                      'and they are AMIV members. This resource is used to '
+                      'log their purchases.'},
+                  'item_lookup': True,
+                  'item_lookup_field': '_id',
+                  'item_url': 'regex("[0-9]+")',
+                  'owner': ['user_id'],
+                  'owner_methods': ['GET'],
+                  'public_item_methods': [],
+                  'public_methods': [],
+                  'registered_methods': [],
+                  'schema': {'_author': {'data_relation': {'embeddable': False,
+                                                           'resource': 'users'},
+                                         'nullable': True,
+                                         'readonly': True,
+                                         'required': False,
+                                         'type': 'objectid',
+                                         'unique': False},
+                             'slot': {'nullable': True,
+                                      'required': False,
+                                      'type': 'integer',
+                                      'unique': False},
+                             'timestamp': {'nullable': True,
+                                           'required': False,
+                                           'type': 'datetime',
+                                           'unique': False},
+                             'type': {'maxlength': 5,
+                                      'nullable': True,
+                                      'required': False,
+                                      'type': 'string',
+                                      'unique': False},
+                             'user_id': {'nullable': True,
+                                         'required': False,
+                                         'type': 'integer',
+                                         'unique': False}}}}
+
 
 def init_app(app):
     """Register resources and blueprints, add hooks and validation."""
