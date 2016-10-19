49d5ddcdd6e36bd32b057f40dfb9aa7104c7410c
Conrad Burchert <conrad.burchert@googlemail.com>
2016-10-19 10:42:02 +0200

    Seperated fixture system from main test class
    The file got pretty long, now it is a mixin. Also custom
    validators can be handles easier by poroviding functions
    to generate the affected fields manually for a fixture

--- amivapi/tests/utils.py
+++ amivapi/tests/utils.py
@@ -6,10 +6,7 @@
 
 import sys
 import json
-import string
-import random
 import unittest
-from datetime import datetime, date
 import os
 from tempfile import mkdtemp
 from itertools import count
@@ -20,10 +17,10 @@ from passlib.context import CryptContext
 from flask import g
 from flask.testing import FlaskClient
 from flask.wrappers import Response
-from eve.methods.post import post_internal
 
+from amivapi.settings import ROOT_PASSWORD
+from amivapi.tests.fixtures import FixtureMixin
 from amivapi import bootstrap, utils
-from amivapi.settings import ROOT_PASSWORD, ROOT_ID, EMAIL_REGEX
 
 
 def find_by_pair(dicts, key, value):
@@ -61,12 +58,6 @@ def is_file_content(path, content):
         return False
 
     return True
-
-
-class BadTestException(Exception):
-    """Exception that can be raised for bad tests."""
-
-    pass
 
 
 class TestClient(FlaskClient):
@@ -129,7 +120,7 @@ class TestResponse(Response):
         return json.loads(self.data.decode())
 
 
-class WebTest(unittest.TestCase):
+class WebTest(unittest.TestCase, FixtureMixin):
     """Base test class for tests against the full WSGI stack.
 
     Inspired by eve standard testing class.
@@ -223,197 +214,6 @@ class WebTest(unittest.TestCase):
                 os.unlink(os.path.join(self.app.config['FORWARD_DIR'], f))
             except Exception as e:
                 print(e)
-
-    def create_random_value(self, definition):
-        """Create a random value for the given cerberus field description."""
-        # If there is a list of allowed values, just pick one
-        if 'allowed' in definition:
-            return random.choice(definition['allowed'])
-
-        t = definition['type']
-        if t == 'string':
-            minimum_length = 0 if definition.get('empty', True) else 1
-            length = random.randint(minimum_length,
-                                    definition.get('maxlength', 100))
-
-            if 'regex' in definition:
-                if definition['regex'] == EMAIL_REGEX:
-                    return "%s@%s.%s" % (
-                        ''.join(random.choice(string.ascii_letters +
-                                              string.digits)
-                                for _ in range(max(1, length - 27))),
-                        ''.join(random.choice(string.ascii_letters +
-                                              string.digits)
-                                for _ in range(20)),
-                        ''.join(random.choice(string.ascii_letters +
-                                              string.digits)
-                                for _ in range(5)))
-                raise NotImplementedError
-
-            return ''.join(random.choice(string.ascii_letters + string.digits)
-                           for _ in range(length))
-
-        elif t == 'boolean':
-            return random.choice([True, False])
-
-        elif t == 'date':
-            return datetime.date.fromordinal(
-                random.randint(0, date.max.toordinal()))
-
-        elif t == 'datetime':
-            return datetime.fromtimestamp(random.randint(0, 2**32))
-
-        elif t == 'float':
-            return random.rand() * random.randint(0, 2**32)
-
-        elif t in ('number', 'integer'):
-            return random.randint(0, 2**32)
-
-        elif t == 'objectid':
-            if 'data_relation' in definition:
-                related_res = definition['data_relation']['resource']
-                return random.choice(list(self.db[related_res].find()))['_id']
-            return ObjectId(''.join(random.choice(string.hexdigits)
-                                    for _ in range(24)))
-
-        raise NotImplementedError
-
-    def preprocess_fixture_object(self, resource, schema, obj, fixture):
-        """Fill in missing fields in a fixture's objects."""
-        if resource == 'users':
-            if 'password' not in obj:
-                # Fill in a password to enable login without ldap in tests
-                obj['password'] = ''.join(
-                    random.choice(string.ascii_letters + string.digits)
-                    for _ in range(30))
-
-        if resource == 'sessions':
-            # We need to fill correct usernames and passwords for sessions, so
-            # they are special
-            if 'username' not in obj:
-                # No username, make a random session
-                obj['username'] = str(random.choice(
-                    list(self.db['users'].find({})))['_id'])
-
-            if 'password' not in obj:
-                username = obj['username']
-                if username in [u'root', str(ROOT_ID)]:
-                    obj['password'] = ROOT_PASSWORD
-                else:
-                    # find the user in the fixture and insert his password
-                    for user in fixture['users']:
-                        if username in (user.get('nethz'),
-                                        user.get('email'),
-                                        str(user.get('_id'))):
-                            obj['password'] = user['password']
-
-            if 'password' not in obj:
-                raise BadTestException(
-                    "Could not determine password for user %s in fixture with "
-                    "unspecified password for session %s"
-                    % (obj['username'], obj))
-
-        # We iterate over the schema to fix missing fields with random values
-        for field, field_def in schema.items():
-            if not (field in obj or
-                    field_def.get('nullable') or field_def.get('readonly')):
-                # We need to add a value for the field to create a valid
-                # object
-                if 'default' in field_def:
-                    obj[field] = field_def['default']
-                else:
-                    # Create a random value
-                    obj[field] = self.create_random_value(field_def)
-
-    def load_fixture(self, fixture):
-        """Load a dictionary as initial database state.
-
-        Missing fields are filled in using defaults, or if not available with
-        random values. Note that this describes post requests, so for example
-        a session will need username and password, not user and token.
-
-        Returns:
-            A list of all created objects
-
-        Example:
-        self.load_fixture({
-            'users': [
-                {
-                    'nethz': 'pablo',
-                    'rfid': '132432'
-                }
-            ],
-            'events': [
-                {
-                    'title': 'mytestevent'
-                }
-            ]
-        })
-        """
-        added_objects = []
-
-        # We need to sort in the order of dependencies. It is for example
-        # not possible to add sessions before we have users, as we need valid
-        # object IDs for the relations.
-        for resource, obj in self.sorted_by_dependencies(fixture):
-            schema = self.app.config['DOMAIN'][resource]['schema']
-
-            # Note that we pass the current state of the fixture to resolve
-            # fields, which depend on already inserted content
-            self.preprocess_fixture_object(resource, schema, obj, fixture)
-
-            # Add it to the database
-            with self.app.test_request_context("/" + resource, method='POST'):
-                response, _, _, return_code = post_internal(resource, obj)
-                if return_code != 201:
-                    raise BadTestException("Fixture could not be loaded:\n%s\n"
-                                           "Problem was caused by:\n%s"
-                                           % (repr(response), obj))
-            added_objects.append(response)
-
-        # Check that everything went fine
-        if len(added_objects) < sum([len(v) for v in fixture.values()]):
-            raise BadTestException("Not all objects in the fixture could be "
-                                   "added! Check your dictionary!")
-
-        return added_objects
-
-    def sorted_by_dependencies(self, fixture):
-        """Sort fixtures by dependencies.
-
-        Generator to yield a fixture in an order, which can be added to the
-        database. It is not possible to add objects which reference other
-        objects before those have been added to the database.
-        Therefore we build a dependency map and yield only objects with
-        resolved dependencies.
-
-        Yields:
-            (resource, object) pairs
-        """
-        deps = {}
-        for resource, resource_def in self.app.config['DOMAIN'].items():
-            deps[resource] = set(
-                field_def.get('data_relation', {}).get('resource')
-                for field_def in resource_def['schema'].values()
-                if 'data_relation' in field_def)
-
-        # Try yielding until all resources have been yielded.
-        while deps:
-            # Search for resource without dependencies
-            no_deps = [res for res in deps if not deps[res]]
-            for resource in no_deps:
-                # Yield all elements of that resource
-                for item in fixture.get(resource, []):
-                    yield (resource, item)
-                # Remove it from the list of resources left
-                deps.pop(resource)
-                # Remove it from dependencies of other resources
-                for dep in deps.values():
-                    dep.discard(resource)
-
-    def new_object(self, resource, **kwargs):
-        """Create a item for resource. Fill in necessary values."""
-        return self.load_fixture({resource: [kwargs]})[0]
 
     # Shortcuts to get a token
     counter = count()
