9f8ca0922443b39598e621bc325ed0f40bd6706b
Tar-Tarus <loubia314@gmail.com>
2016-10-28 04:08:53 -0700

    Corrections to please the PEP8 God of Python Syntax

--- amivapi/joboffers/__init__.py
+++ amivapi/joboffers/__init__.py
@@ -11,65 +11,64 @@ from amivapi.utils import register_domain
 
 
 jobdomain = {
-        'joboffers': {
-            'description': {
-                'fields': {
-                },
-                'general': 'A Job Offer posts repository'
-                'Users can post a job offer with the necessary'
-                'content to fill out a job offer advertisement'
+    'joboffers': {
+        'description': {
+            'fields': {
             },
+            'general': 'A Job Offer posts repository'
+            'Users can post a job offer with the necessary'
+            'content to fill out a job offer advertisement'
+        },
 
-            'resource_methods': ['GET', 'POST'],
-            'item_methods': ['GET', 'PATCH', 'DELETE'],
+        'resource_methods': ['GET', 'POST'],
+        'item_methods': ['GET', 'PATCH', 'DELETE'],
 
-            'public_item_methods': ['GET'],
-            'public_methods': ['GET'],
+        'public_item_methods': ['GET'],
+        'public_methods': ['GET'],
 
-            'schema': {
-                'company': {
-                    'required': True,
-                    'maxlength': 30,
-                    'nullable': True,
-                    'type': 'string',
-                    
-                    # This is basically here, because this is a required
-                    # field. It will make sure there is always either an english
-                    # or a german title
-                    'depends_any': ['title_de', 'title_en']                
-                },
-                'description_de': {
-                    'nullable': True,
-                    'type': 'string',
-                    'unique': False
-                },
-                'description_en': {
-                    'nullable': True,
-                    'type': 'string'
-                },
-                'logo': {
-                    'filetype': ['png', 'jpeg'],
-                    'type': 'media'
-                },
-                'pdf': {
-                    'filetype': ['pdf'],
-                    'type': 'media'
-                },
-                'time_end': {
-                    'nullable': True,
-                    'type': 'datetime'
-                },
-                'title_de': {
-                    'nullable': True,
-                    'type': 'string'
-                },
-                'title_en': {
-                    'nullable': True,
-                    'type': 'string'
-                }
+        'schema': {
+            'company': {
+                'required': True,
+                'maxlength': 30,
+                'nullable': True,
+                'type': 'string',
+                # This is basically here, because this is a required
+                # field. It will make sure there is always either an english
+                # or a german title
+                'depends_any': ['title_de', 'title_en']
+            },
+            'description_de': {
+                'nullable': True,
+                'type': 'string',
+                'unique': False
+            },
+            'description_en': {
+                'nullable': True,
+                'type': 'string'
+            },
+            'logo': {
+                'filetype': ['png', 'jpeg'],
+                'type': 'media'
+            },
+            'pdf': {
+                'filetype': ['pdf'],
+                'type': 'media'
+            },
+            'time_end': {
+                'nullable': True,
+                'type': 'datetime'
+            },
+            'title_de': {
+                'nullable': True,
+                'type': 'string'
+            },
+            'title_en': {
+                'nullable': True,
+                'type': 'string'
             }
         }
     }
+}
 
 
 def init_app(app):
