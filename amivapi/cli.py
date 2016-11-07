e7a9681f44d62d8d95408d3fe7dc57b0cdd6a519
Conrad Burchert <conrad.burchert@googlemail.com>
2016-11-07 21:46:59 +0100

    Added options to manipulate apikeys to cli.py

--- amivapi/cli.py
+++ amivapi/cli.py
@@ -8,7 +8,7 @@
 from os import urandom
 from base64 import b64encode
 from datetime import datetime as dt
-from click import group, option, argument, Path, File
+from click import group, option, argument, Path, File, prompt
 from ruamel import yaml
 
 from amivapi.bootstrap import create_app
@@ -18,6 +18,147 @@ from amivapi.settings import DEFAULT_CONFIG_FILENAME, STORAGE_DIR
 @group()
 def cli():
     """Manage amivapi."""
+
+
+def change_apikey_prompt(config, current_permissions):
+    """ Prompt for changes to the permissions described by current_permissions
+    """
+
+    # We need to create an app to get a list of resources available
+    app = create_app(config) if config else create_app()
+    resources = list(app.config['DOMAIN'].keys())
+
+    while True:
+        print("Current permissions:")
+        print(current_permissions)
+        print("")
+        print("Available resources:")
+        print(", ".join(resources))
+        resource = prompt("Change permissions of what resource? \"x\" to exit",
+                          default="x")
+        if resource == "x":
+            break
+
+        if resource not in resources:
+            print("Unknown resource")
+            continue
+
+        method = prompt("Which method to toggle"
+                        "(GET, POST, PATCH, PUT, DELETE)?", default="")
+
+        if method in ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']:
+            if method in current_permissions.get(resource, []):
+                current_permissions[resource].remove(method)
+                if not current_permissions[resource]:
+                    current_permissions.remove(resource)
+            else:
+                if resource not in current_permissions:
+                    current_permissions[resource] = []
+                current_permissions[resource].append(method)
+        else:
+            print("Unknown method")
+
+
+@cli.command()
+@option("--name", "name", prompt="Key name", help="Name of the key")
+@option("--config", type=Path(exists=True, dir_okay=False, readable=True),
+        default=None,
+        help="use specified config file")
+def apikeys_add(name, config):
+    """ Add a new apikey to the apikey file """
+    try:
+        with open('apikeys.yaml', 'r') as apikey_file:
+            apikeys = yaml.load(apikey_file)
+    except IOError:
+        apikeys = {}
+
+    for token, apikey_data in apikeys.items():
+        if apikey_data['NAME'] == name:
+            print("A key with that name already exists!")
+            return
+
+    token = b64encode(urandom(32)).decode('utf_8')
+
+    current_permissions = {}
+    change_apikey_prompt(config, current_permissions)
+
+    apikeys[token] = {
+        'NAME': name,
+        'PERMISSIONS': current_permissions
+    }
+
+    with open('apikeys.yaml', 'w') as apikey_file:
+        yaml.safe_dump(apikeys, apikey_file, default_flow_style=False)
+
+
+@cli.command()
+@option("--name", "name", prompt="Key name", help="Name of the key")
+def apikeys_delete(name):
+    """ Delete an apikey """
+    try:
+        with open('apikeys.yaml', 'r') as apikey_file:
+            apikeys = yaml.load(apikey_file)
+    except IOError:
+        print("No apikeys set.")
+        return
+
+    token_to_delete = None
+    for token, apikey_data in apikeys.items():
+        if apikey_data['NAME'] == name:
+            token_to_delete = token
+
+    if token_to_delete is None:
+        print("No such key.")
+        return
+
+    del apikeys[token_to_delete]
+
+    with open('apikeys.yaml', 'w') as apikey_file:
+        yaml.safe_dump(apikeys, apikey_file, default_flow_style=False)
+
+
+@cli.command()
+@option("--name", "name", prompt="Key name", help="Name of the key")
+@option("--config", type=Path(exists=True, dir_okay=False, readable=True),
+        default=None,
+        help="use specified config file")
+def apikeys_change(name, config):
+    """ Change the permissions of an existing apikey """
+    try:
+        with open('apikeys.yaml', 'r') as apikey_file:
+            apikeys = yaml.load(apikey_file)
+    except IOError:
+        print("No apikeys set.")
+        return
+
+    token_to_edit = None
+    for token, apikey_data in apikeys.items():
+        if apikey_data['NAME'] == name:
+            token_to_edit = token
+
+    if token_to_edit is None:
+        print("No such key.")
+        return
+
+    change_apikey_prompt(config, apikeys[token_to_edit]['PERMISSIONS'])
+
+    with open('apikeys.yaml', 'w') as apikey_file:
+        yaml.safe_dump(apikeys, apikey_file, default_flow_style=False)
+
+
+@cli.command()
+def apikeys_list():
+    """ Show existing apikeys """
+    try:
+        with open('apikeys.yaml', 'r') as apikey_file:
+            apikeys = yaml.load(apikey_file)
+    except IOError:
+        print("No apikeys set.")
+        return
+
+    for token, apikey_data in apikeys.items():
+        print("%s: %s" % (apikey_data['NAME'], token))
+        print("    %s" % apikey_data['PERMISSIONS'])
 
 
 @cli.command()
