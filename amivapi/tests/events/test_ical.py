8f1baa60331e87a2e42338e13cd29248dbb5f0df
M E <marco.epp@gmx.ch>
2016-11-14 11:53:57 +0100

    Test Coverage // py27 compatible // code cleanup
    - Enlarged Test Coverage of ics_formatter.py to 98%
    - Made tests compatible with python 2.7 (dict.items() vs dict.viewitems())
    - Code of test adheres to flake8 standards now.

--- amivapi/tests/events/test_ical.py
+++ amivapi/tests/events/test_ical.py
@@ -4,14 +4,16 @@
 #          you to buy us beer if we meet and you like the software.
 """Test event GET text/calendar MIME support"""
 
-from icalendar import Calendar, Event
+from icalendar import Calendar
 from json import loads as jloads
 from datetime import datetime, timedelta
+import sys
 
 from amivapi.tests.utils import WebTestNoAuth
 
+
 class EventiCalendarTest(WebTestNoAuth):
-    def test_events_json_ical_3(self):
+    def test_events_json_ical_3_multilang(self):
         ev0 = {
             'title_en': 'party',
             'description_en': 'fun',
@@ -21,7 +23,8 @@ class EventiCalendarTest(WebTestNoAuth):
             'show_infoscreen': True,
             'show_announce': False,
             'time_start': '2016-10-17T18:11:14Z',
-            'time_end': '2016-10-17T22:33:37Z'
+            'time_end': '2016-10-17T22:33:37Z',
+            'location': 'AMIV Office'
         }
         ev1 = {
             'title_en': 'party2',
@@ -51,18 +54,53 @@ class EventiCalendarTest(WebTestNoAuth):
         self.api.post("/events", data=ev2, status_code=201)
 
         # get JSON data and do some simple checks
-        r = jloads(self.api.get("/events", status_code=200).get_data(as_text=True))
+        r = jloads(self.api.get("/events", status_code=200)
+                   .get_data(as_text=True))
         d = r['_items']
         self.assertEqual(len(d), 3)
         # this checks if d[X+1] is a superset of evX (python! yay!)
-        self.assertTrue(ev0.items() <= d[0].items())
-        self.assertTrue(ev1.items() <= d[1].items())
-        self.assertTrue(ev2.items() <= d[2].items())
+        if sys.version_info < (3, 0):
+            self.assertTrue(ev0.viewitems() <= d[0].viewitems())
+            self.assertTrue(ev1.viewitems() <= d[1].viewitems())
+            self.assertTrue(ev2.viewitems() <= d[2].viewitems())
+        else:
+            self.assertTrue(ev0.items() <= d[0].items())
+            self.assertTrue(ev1.items() <= d[1].items())
+            self.assertTrue(ev2.items() <= d[2].items())
         # save uids for comparison with iCal
         etaglist = [x['_etag'] for x in d]
 
         # get iCalendar data and check if etag matches uid
-        r = self.api.get("/events", headers={'Accept': 'text/calendar'}, status_code=200).get_data(as_text=True)
+
+        # no specified language case
+        r = self.api.get("/events", headers={'Accept': 'text/calendar'},
+                         status_code=200).get_data(as_text=True)
+        g = Calendar.from_ical(r)
+        self.assertEqual(g['version'], '2.0')
+        count = 0
+        for c in g.walk():
+            if c.name == "VEVENT":
+                self.assertTrue(str(c.get('uid')) in etaglist)
+                count += 1
+        self.assertEqual(count, len(etaglist))
+
+        # swiss german desired
+        r = self.api.get("/events", headers={'Accept': 'text/calendar',
+                         'Accept-Language': 'de-CH'},
+                         status_code=200).get_data(as_text=True)
+        g = Calendar.from_ical(r)
+        self.assertEqual(g['version'], '2.0')
+        count = 0
+        for c in g.walk():
+            if c.name == "VEVENT":
+                self.assertTrue(str(c.get('uid')) in etaglist)
+                count += 1
+        self.assertEqual(count, len(etaglist))
+
+        # us-english desired
+        r = self.api.get("/events", headers={'Accept': 'text/calendar',
+                         'Accept-Language': 'en-US'},
+                         status_code=200).get_data(as_text=True)
         g = Calendar.from_ical(r)
         self.assertEqual(g['version'], '2.0')
         count = 0
@@ -88,14 +126,19 @@ class EventiCalendarTest(WebTestNoAuth):
         self.api.post("/events", data=ev0, status_code=201)
 
         # get JSON data and do some simple checks
-        r = jloads(self.api.get("/events", status_code=200).get_data(as_text=True))
+        r = jloads(self.api.get("/events", status_code=200)
+                   .get_data(as_text=True))
         d = r['_items']
         self.assertEqual(len(d), 1)
         # check if data matches
-        self.assertTrue(ev0.items() <= d[0].items())
+        if sys.version_info < (3, 0):
+            self.assertTrue(ev0.viewitems() <= d[0].viewitems())
+        else:
+            self.assertTrue(ev0.items() <= d[0].items())
 
         # get iCalendar data and check if etag matches uid
-        r = self.api.get("/events", headers={'Accept': 'text/calendar'}, status_code=200).get_data(as_text=True)
+        r = self.api.get("/events", headers={'Accept': 'text/calendar'},
+                         status_code=200).get_data(as_text=True)
         g = Calendar.from_ical(r)
         self.assertEqual(g['version'], '2.0')
         count = 0
@@ -119,14 +162,19 @@ class EventiCalendarTest(WebTestNoAuth):
         self.api.post("/events", data=ev0, status_code=201)
 
         # get JSON data and do some simple checks
-        r = jloads(self.api.get("/events", status_code=200).get_data(as_text=True))
+        r = jloads(self.api.get("/events", status_code=200)
+                   .get_data(as_text=True))
         d = r['_items']
         self.assertEqual(len(d), 1)
         # check if data matches
-        self.assertTrue(ev0.items() <= d[0].items())
+        if sys.version_info < (3, 0):
+            self.assertTrue(ev0.viewitems() <= d[0].viewitems())
+        else:
+            self.assertTrue(ev0.items() <= d[0].items())
 
         # get iCalendar data and check if is empty as expected
-        r = self.api.get("/events", headers={'Accept': 'text/calendar'}, status_code=200).get_data(as_text=True)
+        r = self.api.get("/events", headers={'Accept': 'text/calendar'},
+                         status_code=200).get_data(as_text=True)
         g = Calendar.from_ical(r)
         self.assertEqual(g['version'], '2.0')
         count = 0
@@ -151,21 +199,27 @@ class EventiCalendarTest(WebTestNoAuth):
         self.api.post("/events", data=ev0, status_code=201)
 
         # get JSON data and do some simple checks
-        r = jloads(self.api.get("/events", status_code=200).get_data(as_text=True))
+        r = jloads(self.api.get("/events", status_code=200)
+                   .get_data(as_text=True))
         d = r['_items']
         self.assertEqual(len(d), 1)
         # check if data matches
-        self.assertTrue(ev0.items() <= d[0].items())
+        if sys.version_info < (3, 0):
+            self.assertTrue(ev0.viewitems() <= d[0].viewitems())
+        else:
+            self.assertTrue(ev0.items() <= d[0].items())
 
         # get iCalendar data and check if is empty as expected
-        r = self.api.get("/events", headers={'Accept': 'text/calendar'}, status_code=200).get_data(as_text=True)
+        r = self.api.get("/events", headers={'Accept': 'text/calendar'},
+                         status_code=200).get_data(as_text=True)
         g = Calendar.from_ical(r)
         self.assertEqual(g['version'], '2.0')
         count = 0
         for c in g.walk():
             if c.name == "VEVENT":
                 self.assertEqual(c.get('dtstart').dt, testtime)
-                self.assertEqual(c.get('dtend').dt, testtime+timedelta(minutes=60))
+                self.assertEqual(c.get('dtend').dt, testtime
+                                 + timedelta(minutes=60))
                 count += 1
         self.assertEqual(count, 1)
 
@@ -185,16 +239,22 @@ class EventiCalendarTest(WebTestNoAuth):
         self.api.post("/events", data=ev0, status_code=201)
 
         # get JSON data and do some simple checks
-        r = jloads(self.api.get("/events", status_code=200).get_data(as_text=True))
+        r = jloads(self.api.get("/events", status_code=200)
+                   .get_data(as_text=True))
         d = r['_items']
         self.assertEqual(len(d), 1)
         # check if data matches
-        self.assertTrue(ev0.items() <= d[0].items())
+        if sys.version_info < (3, 0):
+            self.assertTrue(ev0.viewitems() <= d[0].viewitems())
+        else:
+            self.assertTrue(ev0.items() <= d[0].items())
         ev_id = d[0]['_id']
         ev_etag = d[0]['_etag']
 
         # get iCalendar data and check if etag matches uid
-        r = self.api.get('/events/'+str(ev_id), headers={'Accept': 'text/calendar'}, status_code=200).get_data(as_text=True)
+        r = self.api.get('/events/'+str(ev_id),
+                         headers={'Accept': 'text/calendar'},
+                         status_code=200).get_data(as_text=True)
         g = Calendar.from_ical(r)
         self.assertEqual(g['version'], '2.0')
         count = 0
