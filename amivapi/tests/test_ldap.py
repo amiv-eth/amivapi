e2ff30aab17828ac2577ab12110b31ff7c10aed9
Alexander Dietmüller <alexander@dietmueller.de>
2017-04-17 00:07:12 +0200

    LDAP version number 3
    First of all, the structure of ldap.py is now simplified. The class is dropped
    and replaced by a few module level functions which are easier to understand.
    
    Next the functions themselves have been rearranged to be easier to test.
    Some premature optimization has been removed: The updating of all users
    now uses the same function as the one for one user only, since there were not
    really noticable performance differences. Again, this simplifies testing.
    
    Speaking of testing: Using mock, all ldap functions are now tested.
    There still are some integration tests to verify everything actually works with the
    eth ldap.
    
    Finally a few changes in bootstrap, cli and sessions to use the new functions.

--- amivapi/tests/test_ldap.py
+++ amivapi/tests/test_ldap.py
@@ -11,44 +11,32 @@ There is another file, "ldap_integration.py", which can be used to test
 integration with the real ldap. More info there.
 """
 
-from unittest.mock import MagicMock, patch
+from mock import MagicMock, patch, call
 
 from amivapi import ldap
-from amivapi.tests.utils import WebTest, WebTestNoAuth
-
-MOCK_LDAP_DATA = {
-    'testuser1': {
-        'password': 'testpass1',
-
-    },
-    'testuser2': {
-        'password': 'testpass2',
-
-    }
-}
-
-EXPECTED_RESULTS = {
-    'testuser1': {
-
-    },
-    'testuser2': {
-
-    },
-
-}
+from amivapi.tests.utils import WebTestNoAuth
 
 
-class LdapAuthTest(WebTest):
-    """Tests for LDAP auth."""
+class LdapTest(WebTestNoAuth):
+    """Tests for LDAP with a mock connection."""
 
     def setUp(self, *args, **kwargs):
         """Extended setUp, enable LDAP and replace it with a Mock object."""
-        self.test_config['ENABLE_LDAP'] = True
-        self.test_config['LDAP_USER'] = self.test_config['LDAP_PASS'] = "Bla."
-        self.mock_ldap = MagicMock()
-        # When instantiated, it will return the mock object to use in the tests
-        ldap.AuthenticatedLdap = MagicMock(return_value=self.mock_ldap)
-        super(LdapAuthTest, self).setUp(*args, **kwargs)
+        super(LdapTest, self).setUp(*args, **kwargs)
+
+        self.app.config['ENABLE_LDAP'] = True
+        self.mock_ldap = self.app.config['ldap_connector'] = MagicMock()
+
+    def test_init_app(self):
+        """Test that init app uses correct credentials stored connector."""
+        self.app.config['LDAP_USER'] = 'test'
+        self.app.config['LDAP_PASS'] = 'T3ST'
+        to_patch = 'amivapi.ldap.AuthenticatedLdap'
+        with patch(to_patch, return_value='initialized') as init:
+            ldap.init_app(self.app)
+
+            init.assert_called_with('test', 'T3ST')
+            self.assertEqual(self.app.config['ldap_connector'], 'initialized')
 
     def test_ldap_auth(self):
         """Test that ldap can authenticate a user."""
@@ -62,7 +50,7 @@ class LdapAuthTest(WebTest):
 
         # We patch sync one to return an existing user
         # sync one is tested separately
-        sync = 'amivapi.ldap.LdapConnector.sync_one'
+        sync = 'amivapi.ldap.sync_one'
         _id = {'_id': self.new_object('users')["_id"]}
         with patch(sync, return_value=_id) as patched_sync:
             # Auth with ldap succeeds
@@ -73,21 +61,16 @@ class LdapAuthTest(WebTest):
             # But we will assert that sync_one is called for successful auth
             patched_sync.assert_called()
 
-
-class LdapTest(WebTestNoAuth):
-    """Tests for various LDAP functions like sync."""
-
     def test_escape(self):
         """Test proper escaping of all characters."""
-        to_escape = "*()\\" + chr(0)
-        expected = r"\\2A\\28\\29\\5C\\00"
+        to_escape = "thisisok*()\\" + chr(0)
+        expected = r"thisisok\\2A\\28\\29\\5C\\00"
 
         self.assertEqual(ldap._escape(to_escape), expected)
 
-
-    def test_filter(self):
-        """The received LDAP data can look weird. Test that it is filtered."""
-        ldap_data = {
+    def fake_ldap_data(self, **kwargs):
+        """Produce basic fake ldap data to test filter."""
+        data = {
             'cn': ['pablo'],
             'swissEduPersonMatriculationNumber': '01234567',
             'givenName': ['P'],
@@ -97,223 +80,196 @@ class LdapTest(WebTestNoAuth):
                    'Informationstechnologie und Elektrotechnik'],
             'some_random_field': 'abc',
         }
+        data.update(kwargs)
+        return data
 
-        with self.app.app_context():
-            filtered = ldap._filter_data(ldap_data)
-
-        expected = {
+    def fake_filtered_data(self):
+        """Some data that is a valid output of _filter_data."""
+        return {
             'nethz': 'pablo',
+            'email': 'pablo@ethz.ch',  # this will be auto-generated
             'firstname': 'P',
             'lastname': 'Ablo',
             'department': 'itet',
-            'membership': 'regular'
+            'membership': 'regular',
+            'gender': 'female',
+            'legi': '01234567'
         }
 
-        self.assertEqual(filtered, expected)
-
-'''
-
-# No Pablo in database yet
-        token = self.get_root_token()
-        self.api.get('/users/pablo', status_code=404, token=token)
-
-        # We need to be able to find, authenticate and sync the user
-        ldap_user = {
-            'cn': ['pablo'],
-            'swissEduPersonMatriculationNumber': '01234567',
-            'givenName': ['P'],
-            'sn': ['Ablo'],
-            'swissEduPersonGender': '0',
-            'ou': ['VSETH Mitglied']
-        }
-
-        self.mock_ldap.search = Mock(return_value=[ldap_user])
-
-
-
-
-    def setUp(self, *args, **kwargs):
-        """Extended setUp, replace the ldap in ldap_connector.
-        """
-        self.test_config['ENABLE_LDAP'] = True
-        ldap_connector.ldap = MockLdap()
-
-        super(LdapTest, self).setUp(*args, **kwargs)
-
-
-
-    def test_authenticate_user(self):
-        """Assert authentication is successful."""
+    def test_filter_data(self):
+        """The received LDAP data can look weird. Test that it is filtered."""
         with self.app.app_context():
-            self.assertTrue(
-                ldap_connector.authenticate_user('testuser1', 'testpass1'))
+            filtered = ldap._filter_data(self.fake_ldap_data())
+            expected = self.fake_filtered_data()
 
-    def test_find_user(self):
-        """Assert user can be found."""
+            self.assertEqual(filtered, expected)
+
+    def test_filter_gender(self):
+        """ Test parsing of gender field."""
         with self.app.app_context():
-            user = ldap_connector.find_user('testuser1')
-            self.assertIsNotNone(user)
-            self.assertEqual(user, self.data)
+            female_data = self.fake_ldap_data(swissEduPersonGender='0')
+            female_filtered = ldap._filter_data(female_data)
+            self.assertTrue(female_filtered['gender'] == 'female')
 
-    def test_find_members(self):
-        """Assert that find_members works.
+            male_data = self.fake_ldap_data(swissEduPersonGender='1')
+            male_filtered = ldap._filter_data(male_data)
+            self.assertTrue(male_filtered['gender'] == 'male')
 
-        - All imported users are members
-        - Our user is in the list.
-        """
+    def test_filter_department(self):
+        """Test deparment filtering. The 'ou' entry has to be checked."""
         with self.app.app_context():
-            members = list(ldap_connector.find_members())
+            tests = (
+                ('D-ITET', 'itet'),
+                ('D-MAVT', 'mavt'),
+                ('D-Somethingelse', 'other')
+            )
+            for ldap_value, api_value in tests:
+                data = self.fake_ldap_data(ou=[ldap_value])
+                filtered = ldap._filter_data(data)
+                self.assertTrue(filtered['department'] == api_value)
 
-            # Assert something is found
-            self.assertTrue(members)
+    def test_filter_membership(self):
+        """Test membership filtering. The 'ou' entry has to be checked."""
+        with self.app.app_context():
+            # ou must contain 'VSETH Mitglied' and any of the specified 'ou'
+            # values
+            self.app.config['LDAP_MEMBER_OU_LIST'] = [
+                'test_value', 'other_value'
+            ]
 
-            # Assert only members are imported
-            everyone_is_member = all(member['membership'] == 'regular'
-                                     for member in members)
+            tests = (
+                # (ou, expected result)
+                (['VSETH Mitglied', 'test_value'], 'regular'),
+                (['VSETH Mitglied', 'other_value'], 'regular'),
+                # Something missing
+                (['VSETH Mitglied'], 'none'),
+                (['test_value'], 'none'),
+                (['other_value'], 'none'),
+                ([], 'none'),
+            )
 
-            self.assertTrue(everyone_is_member)
+            for (ou_values, result) in tests:
+                data = self.fake_ldap_data(ou=ou_values)
+                filtered = ldap._filter_data(data)
+                self.assertTrue(filtered['membership'] == result)
 
-            # Assert user is found
-            user_data = [member for member in members
-                         if member['nethz'] == self.data['nethz']]
-            self.assertEqual(user_data, self.data)
+    def test_create_user(self):
+        """Test the 'create' part of _create_or_update_user."""
+        new_user = self.fake_filtered_data()
 
-    def test_compare_and_update(self):
-        """Assert that compare_and_update only changes necessary fields."""
-        fixtures = self.load_fixture({
-            'users': [{'firstname': u'db', 'lastname': u'db'}]})
-        user_id = fixtures[0]['_id']
-
-        with self.app.test_request_context():
-            # Fake db data, only firstname should change
-            fake_db_data = {'_id': user_id,
-                            'firstname': 'db',
-                            'lastname': 'ldap'}
-            fake_ldap_data = {'firstname': 'ldap',
-                              'lastname': 'ldap'}
-
-            ldap_connector.compare_and_update(fake_db_data, fake_ldap_data)
-
-            # Get user from database and compare
-            user = self.db['users'].find_one({'_id': user_id})
-            self.assertEqual(user['firstname'], 'ldap')
-            self.assertEqual(user['lastname'], 'db')
-
-    def test_update_only_upgrades_membership(self):
-        """Assert that the update only changes membership if none."""
-        data_no_change = {'_id': 24 * '0',
-                          'nethz': 'nochange', 'membership': u"honorary"}
-        data_change = {'_id': 24 * '1',
-                       'nethz': 'change', 'membership': u"none"}
-        self.load_fixture({'users': [data_no_change, data_change]})
+        # User not found yet
+        self.api.get('/users/%s' % new_user['nethz'], status_code=404)
 
         with self.app.test_request_context():
-            ldap_data = {'membership': u'regular'}
+            ldap._create_or_update_user(new_user)
 
-            ldap_connector.compare_and_update(data_no_change, ldap_data)
-            ldap_connector.compare_and_update(data_change, ldap_data)
+        # User exists now
+        self.api.get('/users/%s' % new_user['nethz'], status_code=200)
 
-            user_no_change = self.db['users'].find_one({'nethz': 'nochange'})
-            user_change = self.db['users'].find_one({'nethz': 'change'})
-            self.assertEqual(user_no_change['membership'],
-                             data_no_change['membership'])
-            self.assertEqual(user_change['membership'],
-                             ldap_data['membership'])
+    def test_update_user(self):
+        """Test the 'patch' part of _create_or_patch_user."""
+        # The user is in the database. Now change a few things and verify
+        # patch with original data does the correct thing
+        tests = (
+            # (field, db_value, ldap_value, change_expected)
+            ('firstname', 'old', 'new', True),
+            ('lastname', 'old', 'new', True),
+            ('department', 'mavt', 'itet', True),
+            ('gender', 'male', 'female', True),
+            ('legi', '76543210', '01234567', True),
+            # Membership is only upgraded
+            ('membership', 'none', 'regular', True),
+            ('membership', 'regular', 'none', False),
+            ('membership', 'honorary', 'regular', False),
+            ('membership', 'honorary', 'none', False),
+            ('membership', 'extraordinary', 'regular', False),
+            ('membership', 'extraordinary', 'none', False),
+            # email will not be changed
+            ('email', 'old@mail.de', 'new@mail.de', False)
+        )
 
-    def test_update_doesnt_change_email(self):
-        """Assert that mail is never changed."""
-        data_no_change = {'nethz': 'nochange', 'email': u"any@thing.ch"}
-        self.load_fixture({'users': [data_no_change]})
+        for ind, (field, db_value, ldap_value, change) in enumerate(tests):
+            # Create a new user for every test
+            self.new_object('users', nethz=str(ind), **{field: db_value})
+            ldap_data = {'nethz': str(ind), field: ldap_value}
 
-        with self.app.test_request_context():
-            ldap_data = {'email': u'some@thing.ch'}
+            with self.app.test_request_context():
+                result = ldap._create_or_update_user(ldap_data)
 
-            ldap_connector.compare_and_update(data_no_change, ldap_data)
+            if change:
+                self.assertEqual(result[field], ldap_value)
+            else:
+                self.assertEqual(result[field], db_value)
 
-            user_no_change = self.db['users'].find_one({'nethz': 'nochange'})
-            self.assertEqual(user_no_change['email'],
-                             data_no_change['email'])
+    def test_search(self):
+        """Test that ldap is correctly queried."""
+        test_query = "äüáíðáßðöó"
+        attr = [
+            'cn',
+            'swissEduPersonMatriculationNumber',
+            'givenName',
+            'sn',
+            'swissEduPersonGender',
+            'ou'
+            ]
+        mock_results = [1, 2, 3]
+        # Mock ldap query
 
-    def test_sync_one_import(self):
-        """Assert synchronizing one user works."""
-        with self.app.test_request_context():
-            user = ldap_connector.sync_one(self.data['nethz'])
+        mock_search = MagicMock(return_value=mock_results)
+        self.app.config['ldap_connector'].search = mock_search
+        # Mock _filter_data to check results
+        with patch('amivapi.ldap._filter_data') as mock_filter:
+            with self.app.app_context():
+                result = ldap._search(test_query)
 
-            # Double check with database
-            check_user = self.db['users'].find_one(
-                {'nethz': self.data['nethz']})
+                # Verify correct query
+                mock_search.assert_called_with(test_query, attributes=attr)
 
-            # Compare by key since self.data doesnt have fields with _
-            for key in self.data:
-                self.assertEqual(user[key], self.data[key])
-                self.assertEqual(check_user[key], self.data[key])
+                # Assert _filter_data is called with ldap results
+                for ind, _ in enumerate(result):
+                    mock_filter.assert_called_with(mock_results[ind])
 
-    def test_sync_one_update(self):
-        """Same as before, but the user exists now before ldap sync."""
-        self.load_fixture({'users': [{'nethz': self.data['nethz'],
-                                      'membership': u"none"}]})
+    def test_sync_one_found(self):
+        """Sync one queries ldap and creates user."""
+        search_results = (i for i in [1])  # Mock generator
+        search = 'amivapi.ldap._search'
+        create = 'amivapi.ldap._create_or_update_user'
+        with patch(search, return_value=search_results) as mock_search:
+            with patch(create, return_value=2) as mock_create:
+                query = "Abcsdi123"
+                result = ldap.sync_one(query)
 
-        with self.app.test_request_context():
-            user = ldap_connector.sync_one(self.data['nethz'])
+                self.assertEqual(result, 2)
+                mock_search.assert_called_with('(cn=%s)' % query)
+                mock_create.assert_called_with(1)
 
-            # Double check with database
-            check_user = self.db['users'].find_one(
-                {'nethz': self.data['nethz']})
+    def test_sync_one_no_results(self):
+        """Test if sync one return None if there are no results."""
+        search_results = (i for i in [])  # Mock generator
+        search = 'amivapi.ldap._search'
+        create = 'amivapi.ldap._create_or_update_user'
+        with patch(search, return_value=search_results):
+            with patch(create) as mock_create:
+                query = "query"
+                result = ldap.sync_one(query)
 
-            # Compare by key since self.data doesnt have fields with
-            # email won't be changed, so don't check that
-            keys_except_mail = (key for key in self.data if key != "email")
-            for key in keys_except_mail:
-                self.assertEqual(user[key], self.data[key])
-                self.assertEqual(check_user[key], self.data[key])
+                self.assertEqual(result, None)
+                mock_create.assert_not_called()
 
     def test_sync_all(self):
-        """Test sync all imports users by checking the test user."""
-        with self.app.test_request_context():
-            ldap_connector.sync_all()
+        """Test if sync_all contructs the query correctly and creates users."""
+        # Make ou list shorter
+        self.app.config['LDAP_MEMBER_OU_LIST'] = ['a', 'b']
+        expected_query = '(& (ou=VSETH Mitglied) (| (ou=a)(ou=b)) )'
+        search_results = (i for i in [1, 2])
+        search = 'amivapi.ldap._search'
+        create = 'amivapi.ldap._create_or_update_user'
 
-            # Find test user
-            user = self.db['users'].find_one({'nethz': self.data['nethz']})
+        with patch(search, return_value=search_results) as mock_search:
+            with patch(create, return_value=3) as mock_create:
+                with self.app.app_context():
+                    result = ldap.sync_all()
 
-            for key in self.data:
-                self.assertEqual(user[key], self.data[key])
-
-    def test_sync_all_update(self):
-        """Same as before, but the user exists now before ldap sync."""
-        self.load_fixture({'users': [{'nethz': self.data['nethz'],
-                                      'membership': u"none"}]})
-
-        with self.app.test_request_context():
-            ldap_connector.sync_all()
-
-            # Find test user
-            user = self.db['users'].find_one({'nethz': self.data['nethz']})
-
-            # email won't be changed, so don't check that
-            keys_except_mail = (key for key in self.data if key != "email")
-            for key in keys_except_mail:
-                self.assertEqual(user[key], self.data[key])
-
-    def test_import_on_login(self):
-        """Test that login imports the user."""
-        data = {'username': self.data['nethz'], 'password': self.password}
-        self.api.post("/sessions", data=data, status_code=201)
-
-        user = self.db['users'].find_one({'nethz': self.data['nethz']})
-        self.assertIsNotNone(user)
-
-    def test_update_on_login(self):
-        """Test login again, but this time the user already exists."""
-        self.load_fixture({'users': [{'nethz': self.data['nethz'],
-                                      'membership': u"none"}]})
-
-        data = {'username': self.data['nethz'], 'password': self.password}
-        self.api.post("/sessions", data=data, status_code=201)
-
-        # Comparison necessary to make sure it was updated
-        # email won't be changed, so don't check that
-        user = self.db['users'].find_one({'nethz': self.data['nethz']})
-        keys_except_mail = (key for key in self.data if key != "email")
-        for key in keys_except_mail:
-            self.assertEqual(user[key], self.data[key])
-'''
+                mock_search.assert_called_with(expected_query)
+                mock_create.assert_has_calls([call(1), call(2)])
+                self.assertEqual(result, [3, 3])
