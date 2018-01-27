cdb61de27a5be7956adfe76d93f6b49759376b6e
Conrad Burchert <conrad.burchert@googlemail.com>
2018-01-27 11:29:07 +0100

    GET to /users now projects a password_set field

--- amivapi/users/security.py
+++ amivapi/users/security.py
@@ -105,6 +105,22 @@ def hide_fields(response):
                     item.pop(key)
 
 
+# Project password status
+
+def project_password_status(response):
+    """Add a boolean field password_state to the response.
+
+    Args:
+        response: Response object of the request
+    """
+    # Compatibility with both item and resource hook
+    items = response.get('_items', [response])
+
+    for item in items:
+        item['password_set'] = item.get('password') is not None
+        print(item)
+
+
 # Password hashing
 
 def _hash_password(user):
