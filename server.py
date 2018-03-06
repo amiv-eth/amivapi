cb55cd7e4bb86da9472e52e362e779096fb29163
Alexander Dietmüller <alexander@dietmueller.de>
2018-03-06 20:52:56 +0100

    Update README and polish server.py a little

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-# [bjoern](https://github.com/jonashaag/bjoern) uwsgi server to server app
-# Used to serve amivapi in Docker container.
+# wsgi server (used in docker container)
+# [bjoern](https://github.com/jonashaag/bjoern) required.
 
 from amivapi import create_app
 import bjoern
 
-print('Starting bjoern...')
-bjoern.run(create_app(), '0.0.0.0', 8080)
+if __name__ == '__main__':
+    print('Starting bjoern on port 8080...', flush=True)
+    bjoern.run(create_app(), '0.0.0.0', 8080)
