6e625ee25fd3966d3c32c4054d2fe93166d3125b
Alexander Dietmüller <adietmue@ethz.ch>
2018-11-23 15:04:35 +0100

    style fixes

--- amivapi/documentation/update_documentation.py
+++ amivapi/documentation/update_documentation.py
@@ -6,8 +6,8 @@
 
 Eve-Swagger provides a useful baseline, but is missing many details.
 
-Furthermore, as of this commit, development seems slow with multiple open pull 
-requests without responses. Thus we have decided to update the documentation 
+Furthermore, as of this commit, development seems slow with multiple open pull
+requests without responses. Thus we have decided to update the documentation
 manually. Should Eve-Swagger development gain traction again, we could try to
 integrate these changes.
 """
