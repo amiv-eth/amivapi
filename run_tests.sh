e350942830e2612ad743479ad1fff14518298ba4
Conrad Burchert <conrad.burchert@googlemail.com>
2019-03-31 13:37:40 +0200

    Speed up testing in docker 10x

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker build -t amivapi-testrunner -f Dockerfile.test . || exit 1
-docker run --rm -it --name testrunner amivapi-testrunner $@
+docker run --rm -it --name testrunner --mount type=tmpfs,destination=/tmp \
+    amivapi-testrunner $@
