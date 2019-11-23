8363785d651900b5b7a7ad838064b256c51e5435
Conrad Burchert <conrad.burchert@googlemail.com>
2019-11-23 23:19:18 +0100

    Fix deprecation warnings. (#409)

--- amivapi/groups/model.py
+++ amivapi/groups/model.py
@@ -316,7 +316,7 @@ groupdomain = {
                 },
 
                 'type': 'dict',
-                'keyschema': {'type': 'string',
+                'keysrules': {'type': 'string',
                               'api_resources': True},
                 'valuesrules': {'type': 'string',
                                 'allowed': ['read', 'readwrite']},
