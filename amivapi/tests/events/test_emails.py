8d0218865ae800b5a8fe17a6893f1ab3b04c3a60
Sandro Lutz <code@temparus.ch>
2020-03-05 23:23:45 +0100

    Fix issue for sending event signup confirm email
    Ensures that a confirm email is sent to an event signup by email even
    when "user" is set to null in the request payload.

--- amivapi/tests/events/test_emails.py
+++ amivapi/tests/events/test_emails.py
@@ -41,6 +41,36 @@ class EventMailTest(WebTestNoAuth):
                               status_code=200).json
         self.assertEqual(signup['confirmed'], True)
 
+    def test_email_token_when_user_null(self):
+        """Test confirmation by email link when user is set to None."""
+        event = self.new_object('events', spots=100, allow_email_signup=True)
+        signup = self.api.post('/eventsignups', data={
+            'event': str(event['_id']),
+            'user': None,
+            'email': 'bla@test.bla'
+        }, status_code=201).json
+
+        self.assertEqual(signup['confirmed'], False)
+        self.assertEqual(len(self.app.test_mails), 1)
+
+        # Look for sent out mail
+        mail = self.app.test_mails[0]
+        self.assertEqual(mail['receivers'][0], 'bla@test.bla')
+
+        # Use the confirm link
+        token = re.search(r'/confirm_email/(.+)\n\n', mail['text']).group(1)
+        # With redirect set
+        self.app.config['EMAIL_CONFIRMED_REDIRECT'] = "somewhere"
+        self.api.get('/confirm_email/%s' % token, status_code=302)
+        # And without
+        self.app.config.pop('EMAIL_CONFIRMED_REDIRECT')
+        self.api.get('/confirm_email/%s' % token, status_code=200)
+
+        # Check that the signup got confirmed
+        signup = self.api.get('/eventsignups/%s' % signup['_id'],
+                              status_code=200).json
+        self.assertEqual(signup['confirmed'], True)
+
     def test_email_signup_delete(self):
         """Test deletion of signup via email link."""
         event = self.new_object('events', spots=100, selection_strategy='fcfs')
