998bbdf158908a14009da77c86b1b0c263c3d04e
Dominique Portenier <75583205+d-portenier@users.noreply.github.com>
2022-05-11 09:59:43 +0200

    Code Cleanup
    Co-authored-by: Sandro Lutz <temparus@users.noreply.github.com>

--- amivapi/tests/users/test_user_cleanup.py
+++ amivapi/tests/users/test_user_cleanup.py
@@ -17,60 +17,51 @@ class UserCleanupTest(utils.WebTest):
     """Test automatic User Cleanup"""
 
     def test_autodelete(self):
+        cleanup_time = "2019-12-24T14:15:22Z"
+
         entries = [
             {
-                'nethz': 'pablamiv',
-                'firstname': "Pabla",
-                'lastname': "AMIV",
-                'email': "pabla@amiv.ch",
-                'gender': 'male',
-                'membership': 'none'
+                # Non-member updated 6 months prior to cleanup.
+                'membership': 'none',
+                'updated': '2019-08-24T14:15:22Z'
             },
             {
-                'nethz': 'pablomiv',
-                'firstname': "Pablo",
-                'lastname': "AMIV",
-                'email': "pablo@amiv.ch",
-                'gender': 'male',
-                'membership': "regular"
+                # Regular member updated 6 months prior to cleanup.
+                'membership': 'regular',
+                'updated': '2019-08-24T14:15:22Z'
             },
             {
-                'nethz': 'pablemiv',
-                'firstname': "Pable",
-                'lastname': "AMIV",
-                'email': "pable@amiv.ch",
-                'gender': 'male',
-                'membership': "none"
+                # Non-member updated 18 months prior to cleanup.
+                # This use should be removed during cleanup.
+                'membership': 'none',
+                'updated': '2018-08-24T14:15:22Z'
             },
             {
-                'nethz': 'pablimiv',
-                'firstname': "Pabli",
-                'lastname': "AMIV",
-                'email': "pabli@amiv.ch",
-                'gender': 'male',
-                'membership': "honorary"
+                # Honorary member updated 18 months prior to cleanup.
+                'membership': 'honorary',
+                'updated': '2018-08-24T14:15:22Z'
             }
         ]
 
         root_token = self.get_root_token()
 
         with self.app.app_context():
-            with freeze_time("2019-08-24T14:15:22Z"):
-                # Non-member, updated 6 Month ago
-                self.api.post("/users", data=entries[0], token=root_token)
-                # Member, updated 6 Month ago
-                self.api.post("/users", data=entries[1], token=root_token)
-
-            with freeze_time("2018-08-24T14:15:22Z"):
-                # Non-member, updated 18 Month ago (should be deleted)
-                self.api.post("/users", data=entries[2], token=root_token)
-                # (honorary) Member, updated 18 Month ago
-                self.api.post("/users", data=entries[3], token=root_token)
+            for idx, entry in enumerate(entries):
+                with freeze_time(entry.get('updated')):
+                    user = {
+                        'nethz': f'pabloamiv{str(idx)}',
+                        'firstname': 'Pablo',
+                        'lastname': "AMIV",
+                        'email': f'pablo{str(idx)}@amiv.ch',
+                        'gender': 'male',
+                        'membership': entry.get('membership')
+                    }
+                    self.api.post("/users", data=user, token=root_token)
 
             users = self.api.get("/users", token=root_token).json
             self.assertEqual(len(users['_items']), 4)
 
-            with freeze_time("2019-12-24T14:15:22Z"):
+            with freeze_time(cleanup_time):
                 cleanup.remove_inactive_users()
 
             users = self.api.get("/users", token=root_token).json
