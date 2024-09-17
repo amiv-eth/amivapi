7a7e75e17b50e97b0464630e1d44a09d88db8619
Johannes Zumthurm <nimro27@gmx.net>
2024-09-17 16:16:14 +0200

    add admin user and group

--- dev_mongoinit.js
+++ dev_mongoinit.js
@@ -11,15 +11,49 @@ db.createUser(
     }
 );
 
-db.getSiblingDB('test_amivapi').createUser(
-    {
-        user: "test_user",
-        pwd: "test_pw",
-        roles: [
-            {
-                role: "readWrite",
-                db: "test_amivapi"
-            }
-        ]
-    }
-);
+db = db.getSiblingDB('amivapi');
+
+// Create admin user with password admin
+let userId = db.users.insertOne({
+    nethz: 'admin',
+    password: '$pbkdf2-sha256$5$OqfUmtNaq5UyRohxDuGckw$9H/UL5N5dA7JmUq7ohRPfmJ84OUnpRKjTgsMeuFilXM',
+    email: "admin@example.com",
+    membership: "regular",
+    gender: "female",
+    firstname: "ad",
+    lastname: "min",
+    _etag: "27f987fd9dd45d491e5aea3e27730israndom",
+}).insertedId;
+
+// Create admin group with permissions on all resources
+let groupId = db.groups.insertOne({
+    name: 'admin',
+    permissions: {
+        apikeys: "readwrite",
+        users: "readwrite",
+        sessions: "readwrite",
+        events: "readwrite",
+        eventsignups: "readwrite",
+        groups: "readwrite",
+        groupmemberships: "readwrite",
+        joboffers: "readwrite",
+        beverages: "read",
+        studydocuments: "readwrite",
+        oauthclients: "readwrite",
+    },
+    _etag: "27f987fd9dd45d491e5aea3e27730israndom",
+}).insertedId;
+
+// Add admin to admin group
+db.groupmemberships.insertOne({
+    user: userId,
+    group: groupId,
+    _etag: "27f987fd9dd45d491e5aea3e27730israndom",
+})
+
+// Add Local Tool client for admin tool
+db.oauthclients.insertOne({
+    client_id: "Local Tool",
+    redirect_uri: "http://localhost",
+    _etag: "27f987fd9dd45d491e5aea3e27730israndom",
+});
